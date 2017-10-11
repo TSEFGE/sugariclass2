aba5d3fad25911f9ef311e6ce9695f5d360d3eda
Tserrano <tomytree22@hotmail.com>
2017-10-10 22:36:51 -0500

    add

--- config_override.php
+++ config_override.php
@@ -1,5 +1,5 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['disabled_languages'] = '';
+$sugar_config['disabled_languages'] = 'en_us';
 $sugar_config['default_language'] = 'es_ES';
 /***CONFIGURATOR***/
